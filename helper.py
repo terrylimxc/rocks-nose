73f8e8e55767bdbb03baf5c9c546597be4f22521
terrylimxc <terryworld1998@gmail.com>
2022-11-02 02:25:22 +0800

    Add Progress Bar
    Add Progress Bar and print statements to track progress of code execution for users

--- helper.py
+++ helper.py
@@ -4,13 +4,14 @@ import joblib
 import orjson
 import os
 import pandas as pd
+from pathlib import Path
 from imblearn.combine import SMOTETomek
 from imblearn.ensemble import BalancedRandomForestClassifier
 from imblearn.under_sampling import TomekLinks
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.preprocessing import OrdinalEncoder
+from tqdm import tqdm
 
-# from tqdm import tqdm
 from xgboost import XGBClassifier
 
 pd.options.mode.chained_assignment = None
@@ -26,11 +27,21 @@ def parse_data(data_dir):
     ouput: transformed train
     """
     genes = []
-    for line in open(data_dir, "r"):
-        genes.append(orjson.loads(line))
+    with tqdm(total=Path(data_dir).stat().st_size, desc="Reading File") as pbar:
+        with open(data_dir, "rb") as f:
+            for line in f:
+                line = orjson.loads(line)
+                pbar.update(f.tell() - pbar.n)
+                genes.append(line)
+            f.close()
+
+    # with open(data_dir, "r") as f:
+    #     for line in tqdm(f, total=121838, desc="Reading File"):
+    #         genes.append(orjson.loads(line))
+    #     f.close()
 
     lst = []
-    for gene in genes:
+    for gene in tqdm(genes, desc = "Parsing Data"):
         transcript_id = next(iter(gene))
         layer = gene[transcript_id]
         position = next(iter(layer))
@@ -270,6 +281,7 @@ def train(df, method="SmoteTomek", out="model"):
 
     clf.fit(X_train, y_train)
 
+    print("Saving Model")
     filename = "./results/" + out + ".sav"
     pickle.dump(clf, open(filename, "wb"))
 
--- prepare.py
+++ prepare.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
+import dask.dataframe as dd
 import pandas as pd
 from helper import encoder, parse_data, summarise, train
+from tqdm.dask import TqdmCallback
 
 pd.options.mode.chained_assignment = None
 
@@ -33,9 +35,21 @@ def main():
     labels = pd.read_csv(label)
     labels = labels.rename(columns={"transcript_position": "position"})
 
+    # Parse Data
     gene = parse_data(data)
-    full_data = pd.merge(gene, labels, on=["transcript_id", "position"], how="left")
-    summarised = summarise(full_data, flag=True)
+
+    # Merge Data with Labels
+    sd1 = dd.from_pandas(gene, npartitions=3)
+    sd2 = dd.from_pandas(labels, npartitions=3)
+    with TqdmCallback(desc="Adding Labels"):
+        full_data = sd1.merge(sd2, how="left", on=["transcript_id", "position"]).compute()
+
+    # Summarise dataframe, i.e. compress rows
+    print("Summarising Data")
+    summarised = summarise(full_data, method="mean", flag=True)
+
+    # Encode nucleotides
+    print("Encoding Data")
     encoded = encoder(summarised)
     cols = encoded.columns.tolist()
     new_cols = cols[:1] + [cols[-2]] + [cols[-3]] + [cols[-1]] + cols[1:-3]
@@ -44,6 +58,8 @@ def main():
         {"nucleotide-1": "int64", "nucleotide": "int64", "nucleotide+1": "int64"}
     )
 
+    # Prepare & Train model
+    print("Training model")
     # Saved model will be under results directory
     train(encoded, method=model, out=output_name)
 
