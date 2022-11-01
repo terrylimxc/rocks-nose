8285d5e85408f82c75b46f5bceeed79ef3fe81f1
terrylimxc <terryworld1998@gmail.com>
2022-11-01 15:06:43 +0800

    Change filepath
    Remove dependencies of filepath in command line and modify location of saved files

--- predict.py
+++ predict.py
@@ -14,8 +14,8 @@ def main():
 
     args = vars(parser.parse_args())
 
-    data = args["data"]
-    filename = args["model"] + ".sav"
+    data = "./data/" + args["data"]
+    filename = "./results/" + args["model"] + ".sav"
 
     gene = parse_data(data)
     summarised = summarise(gene)
@@ -25,7 +25,7 @@ def main():
     cols = encoded.columns.tolist()
     test_new = cols[:2] + [cols[-2]] + [cols[-3]] + [cols[-1]] + cols[2:-3]
     encoded = encoded[test_new]
-    encoded.to_csv("encoded.csv", index=False)
+
     test_pred = clf.predict_proba(encoded.drop(columns=["transcript_id", "position"]))[
         :, 1
     ]
@@ -33,7 +33,7 @@ def main():
     results["score"] = test_pred
     results = results.rename(columns={"position": "transcript_position"})
 
-    filename = data.split(".json")[0] + ".csv"
+    filename = "./results/" + (data.split(".json")[0]).split("./data/")[-1] + ".csv"
     results.to_csv(filename, index=False)
 
 
