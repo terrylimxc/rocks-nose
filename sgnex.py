e718332211d6459d904dd0d2a13524f179507d0f
terrylimxc <terryworld1998@gmail.com>
2022-11-03 19:34:21 +0800

    Update sgnex.py

--- sgnex.py
+++ sgnex.py
@@ -31,23 +31,23 @@ def main():
     encoded_filename = (data.split(".json")[0]).split("./data/")[-1]
     encoded.to_csv(f"./results/encoded_{encoded_filename}.csv", index=False)
 
-    # Prepare and test model
-    print("Running Model")
-    clf = pickle.load(open(filename, "rb"))
-    cols = encoded.columns.tolist()
-    test_new = cols[:2] + [cols[-2]] + [cols[-3]] + [cols[-1]] + cols[2:-3]
-    encoded = encoded[test_new]
+    # # Prepare and test model
+    # print("Running Model")
+    # clf = pickle.load(open(filename, "rb"))
+    # cols = encoded.columns.tolist()
+    # test_new = cols[:2] + [cols[-2]] + [cols[-3]] + [cols[-1]] + cols[2:-3]
+    # encoded = encoded[test_new]
 
-    test_pred = clf.predict_proba(encoded.drop(columns=["transcript_id", "position"]))[
-        :, 1
-    ]
-    results = encoded[["transcript_id", "position"]]
-    results["score"] = test_pred
-    results = results.rename(columns={"position": "transcript_position"})
+    # test_pred = clf.predict_proba(encoded.drop(columns=["transcript_id", "position"]))[
+    #     :, 1
+    # ]
+    # results = encoded[["transcript_id", "position"]]
+    # results["score"] = test_pred
+    # results = results.rename(columns={"position": "transcript_position"})
 
-    print("Saving Results")
-    filename = "./results/" + (data.split(".json")[0]).split("./data/")[-1] + ".csv"
-    results.to_csv(filename, index=False)
+    # print("Saving Results")
+    # filename = "./results/" + (data.split(".json")[0]).split("./data/")[-1] + ".csv"
+    # results.to_csv(filename, index=False)
 
 
 if __name__ == "__main__":
