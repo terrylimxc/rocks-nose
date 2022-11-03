399d1799dca48d359e041824f6f65b92880bf6e1
terrylimxc <terryworld1998@gmail.com>
2022-11-03 23:34:18 +0800

    Update generate_small_json.py

--- attempts/generate_small_json.py
+++ attempts/generate_small_json.py
@@ -1,33 +1,30 @@
+from pathlib import Path
+
 import json
 import orjson
 from tqdm import tqdm
-from pathlib import Path
 
-genes=[]
-data_dir = "./data/dataset3.json"
-with tqdm(total=Path(data_dir).stat().st_size, desc="Reading File") as pbar:
-    with open(data_dir, "rb") as f:
-        for line in f:
-            line = orjson.loads(line)
-            pbar.update(f.tell() - pbar.n)
-            genes.append(line)
-        f.close()
+
+def read_file(data_dir):
+    genes = []
+    with tqdm(total=Path(data_dir).stat().st_size, desc="Reading File") as pbar:
+        with open(data_dir, "rb") as f:
+            for line in f:
+                line = orjson.loads(line)
+                pbar.update(f.tell() - pbar.n)
+                genes.append(line)
+            f.close()
+    return genes
+
+original_ds3 = read_file("./data/dataset3.json")
 
 # Save first 50 lines to new file
-genes = genes[:50]
+new_ds3 = original_ds3[:50]
 with open("./data/sample.json", "wb") as f:
-    for i in genes:
+    for i in new_ds3:
         f.write(json.dumps(i).encode())
         f.write("\n".encode())
     f.close()
 
 # Check new file can be opened and read
-a=[]
-data_dir = "./data/sample.json"
-with tqdm(total=Path(data_dir).stat().st_size, desc="Reading File") as pbar:
-    with open(data_dir, "rb") as f:
-        for line in f:
-            line = orjson.loads(line)
-            pbar.update(f.tell() - pbar.n)
-            a.append(line)
-        f.close()
+new_ds3 = read_file("./data/sample.json")
